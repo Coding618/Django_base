b5d3fdc2dd52ba083d971317800e5f35a30e7810
Sherlock <sherlock0618@qq.com>
2021-09-05 10:51:17 +0800

    定义模型

--- code/bookmanager/book/models.py
+++ code/bookmanager/book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+    # 默认创建id
+    name = models.CharField(max_length=10)
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键： 人物属于哪本书
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+
