d005fbd216a482a1862cc2a6310ef6d3e1eb82dd
Sherlock <sherlock0618@qq.com>
2021-09-05 11:31:27 +0800

    admin站点

--- code/bookmanager/book/models.py
+++ code/bookmanager/book/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class BookInfo(models.Model):
     # 默认创建id
     name = models.CharField(max_length=10)
-
+    ##重写 str方法 让admin可以显示书籍的名字
+    def __str__(self):
+        return self.name
 
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
