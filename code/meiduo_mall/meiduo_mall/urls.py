bd1ec5962ba62d68571ea4d2d08289ff7ee567e8
Sherlock Wong <867921653@qq.com>
2021-09-26 13:27:58 +0800

    git push

--- code/meiduo_mall/meiduo_mall/urls.py
+++ code/meiduo_mall/meiduo_mall/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     test ctrl + K 来进行git 的 git add commit 的操作
+    git + shift + K 进行 git push 操作
 """
 from django.contrib import admin
 from django.urls import path
