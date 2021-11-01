be9f07cc89682f46c72465c47b9992490dafa1b8
Sherlock Wong <867921653@qq.com>
2021-11-01 15:48:26 +0800

    缓存功能的实现

--- code/meiduo_mall/apps/areas/views.py
+++ code/meiduo_mall/apps/areas/views.py
@@ -21,20 +21,26 @@ from django.views import View
 """
 from apps.areas.models import Area
 from django.http import JsonResponse
+from django.core.cache import cache
 class AreaView(View):
     def get(self, request):
-        # 1. 查询省份信息
-        provinces = Area.objects.filter(parent=None)
+        # 先查缓存数据
+        province_list = cache.get('province')
+        if province_list is None:
+            # 1. 查询省份信息
+            provinces = Area.objects.filter(parent=None)
 
-        # 2. 将对象转换为字典数据
-        parernt_list = []
-        for province in provinces:
-            parernt_list.append({
-                'id': province.id,
-                'name': province.name
-            })
+            # 2. 将对象转换为字典数据
+            province_list = []
+            for province in provinces:
+                province_list.append({
+                    'id': province.id,
+                    'name': province.name
+                })
+            # cache.set(key, value, expire)
+            cache.set('province', province_list, 24*3600)
         # 3. 返回响应
-        return JsonResponse({'code': 0, 'errmsg': 'OK!', 'province_list': parernt_list})
+        return JsonResponse({'code': 0, 'errmsg': 'OK!', 'province_list': province_list})
 
 """
 需求：
@@ -56,15 +62,27 @@ class SubAreaView(View):
         # 1. 获取省份id、市id，查询信息
         # Area.objects.filter(parent_id=id)
         # Area.objects.filter(parent=id)
-
-        up_level = Area.objects.get(id=id)
-        down_level = up_level.subs.all()
-        # 2. 将对象转换为字典数据
-        data_list = []
-        for item in down_level:
-            data_list.append({
-                'id': item.id,
-                'name': item.name
-                 })
+        # 先获取缓存
+        data_list = cache.get('city:%s' % id)
+        if data_list is None:
+            up_level = Area.objects.get(id=id)
+            down_level = up_level.subs.all()
+            # 2. 将对象转换为字典数据
+            data_list = []
+            for item in down_level:
+                data_list.append({
+                    'id': item.id,
+                    'name': item.name
+                     })
+            # 缓存
+            cache.set('city:%s' % id, data_list, 24*3600)
         # 3. 返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok~', 'sub_data': {'subs': data_list}})
+"""
+日活
+注册用户        1亿
+日活用户        1%      1百万
+下单比例        1%      1W  10000
+新增地址的概率   1%      100W  300次    2次
+不经常变化的数据    我们最好缓存到redis（内容）中，减少数据库的查询 
+"""
